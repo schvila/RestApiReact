a412457e897ca914235c6e7dad064831efdeabb3
Standa Chvila <schvila@gmail.com>
2022-06-14 07:18:55 +0200

    ts-node-dev, before local mongodb

--- ServerTS/src/app.ts
+++ ServerTS/src/app.ts
@@ -1,36 +1,35 @@
-const path = require('path');
+const path = require("path");
 
 //const express = require('express');
-import express,{Request, Response,NextFunction} from 'express';
+import express, { Request, Response, NextFunction } from "express";
 
 //const mongoose = require('mongoose');
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 //const multer = require('multer');
-import multer from 'multer';
+import multer from "multer";
 
 // const feedRoutes = require('./routes/feed');
 // const authRoutes = require('./routes/auth');
 
-import feedRoutes from './routes/feed';
-import authRoutes from './routes/auth';
+import feedRoutes from "./routes/feed";
+import authRoutes from "./routes/auth";
 
 const app = express();
 
-
 const fileStorage = multer.diskStorage({
   destination: (req: Request, file: Express.Multer.File, cb) => {
-    cb(null, 'images');
+    cb(null, "images");
   },
   filename: (req: Request, file: Express.Multer.File, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
-  }
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
 });
 
 const fileFilter = (req: Request, file: Express.Multer.File, cb) => {
   if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
   ) {
     cb(null, true);
   } else {
@@ -40,19 +39,18 @@ const fileFilter = (req: Request, file: Express.Multer.File, cb) => {
 app.use(express.urlencoded({ extended: true })); // ommits body parser
 app.use(express.json());
 
-
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
 );
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
@@ -69,13 +67,14 @@ app.use((error: any, req: Request, res: Response, next: NextFunction) => {
 
 mongoose
   .connect(
-    'mongodb+srv://standa:rnEZ3rjw3lf9J7Bh@cluster0.5tfx5.mongodb.net/messages?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
+    "mongodb://localhost:27017/messages?retryWrites=true&w=majority"
+    // {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // }
   )
-  .then(result => {
+  .then((result) => {
+    console.log("Mongoose connect listen on port 8080.");
     app.listen(8080);
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
