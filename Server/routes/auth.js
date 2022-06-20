c1cefc90a1d7d206b7c05732da6a484fa56bd4f7
Standa Chvila <schvila@gmail.com>
2022-06-20 13:24:36 +0200

    verify status

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -15,26 +15,26 @@ router.put(
       .isEmail()
       .withMessage('Please enter a valid email.')
       .custom((value, { req }) => {
-        return User.findOne({ email: value }).then(userDoc => {
+        return User.findOne({ email: value }).then((userDoc) => {
           if (userDoc) {
             return Promise.reject('E-Mail address already exists!');
           }
         });
       })
       .normalizeEmail(),
-    body('password')
-      .trim()
-      .isLength({ min: 5 }),
-    body('name')
-      .trim()
-      .not()
-      .isEmpty()
+    body('password').trim().isLength({ min: 5 }),
+    body('name').trim().not().isEmpty(),
   ],
   authController.signup
 );
 
 router.post('/login', authController.login);
 router.get('/status', isAuth, authController.getUserStatus);
-router.patch("/status", isAuth, authController.updateUserStatus);
+router.patch(
+  '/status',
+  isAuth,
+  [body('status').trim().not().isEmpty()],
+  authController.updateUserStatus
+);
 
 module.exports = router;
